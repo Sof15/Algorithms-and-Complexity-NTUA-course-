200d40f2c9cd3748b8e7e2b120509b46b168c933
Sof15 <sof.galanaki@gmail.com>
2018-11-12 14:01:21 +0200

    fixed particles

--- algo1_2.cpp
+++ algo1_2.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <stack>
-#include <cstdlib>
 
 using namespace std;
 
@@ -10,85 +8,182 @@ struct line{
 	long long int horizIntersect;
 	long double prevInter;
 };
+struct input{
+	long long int t;
+	long long int u;
+	short int relevant;
+};
 
-stack <line> StackA,StackB;
 line particle;
-void printStack(stack <line> s){
-	stack <line> r;
-	while(!s.empty()){
-		cout<<s.top().order<<"\n";
-		r.push(s.top());
-		s.pop();
-	}
-	while(!r.empty()){
-		s.push(r.top());
-		r.pop();
-	}
-}
-stack <line> fillStack(long long int i,long  long int t, long long int u){
-	if(StackA.empty()){
-			particle.order=i+1;
-			particle.slope=u;
-			particle.horizIntersect=-1*t*u;
+long long int top;
+//function for finding the "first" lines and intersections
+void fillStack(line *s, long long int slo, long long int horIn){
+		
+		if(s[0].order==0){		
 			particle.prevInter=0.0;
-			StackA.push(particle);
+			s[0]=particle;
+			top=0;
 		}
-		else if (StackA.top().order==1){
-			particle.prevInter=((-1*t*u-StackA.top().horizIntersect)/(StackA.top().slope-u));
-		//	cout<<" "<<particle.prevInter<<" "<<StackA.top().slope<<" "<<u<<" "<<StackA.top().horizIntersect<<" "<<t;
-			particle.order=i+1;
-			particle.slope=u;
-			particle.horizIntersect=-t*u;
-			StackA.push(particle);
+		else if (s[top].order==1){
+			if(!(abs(s[top].slope)>=abs(slo))){
+				particle.prevInter=((-abs(horIn)+abs(s[top].horizIntersect))/(long double)(abs(s[top].slope)-abs(slo)));
+				s[top+1]=particle;
+				top++;
+			}
 		}		
 		else{
-			//cout<<StackA.top().slope<<" "<<u;
-			//printStack(StackA);
-			if(!(StackA.top().slope>=u)){
+			if(!(abs(s[top].slope)>=abs(slo))){
 				
-				if(StackA.top().prevInter<((-1*t*u-StackA.top().horizIntersect)/(StackA.top().slope-u))){
-					//cout << "foo";
-					particle.order=i+1;
-					particle.slope=u;
-					particle.horizIntersect=-t*u;
-					particle.prevInter=(StackA.top().slope-u)/(StackA.top().horizIntersect+t*u);
-					StackA.push(particle);
+				if(s[top].prevInter<((-abs(horIn)+abs(s[top].horizIntersect))/(long double)(abs(s[top].slope)-abs(slo)))){
+					particle.prevInter=((-abs(horIn)+abs(s[top].horizIntersect))/(long double)(abs(s[top].slope)-abs(slo)));
+					s[top+1]=particle;
+					top++;
 				}
 				else{
-					
-					while(StackA.top().order>1 && StackA.top().prevInter>((-1*t*u-StackA.top().horizIntersect)/(StackA.top().slope-u)))
+					while(s[top].order>1 && s[top].prevInter>((-abs(horIn)+abs(s[top].horizIntersect))/(long double)(abs(s[top].slope)-abs(slo))))
 					{
-						
-						StackA.pop();
-						particle.order=i+1;
-						particle.slope=u;
-						particle.horizIntersect=-t*u;
-						particle.prevInter=(StackA.top().slope-u)/(StackA.top().horizIntersect+t*u);
-						StackA.push(particle);
+						s[top].order=0;
+						top--;		
 					}
+					particle.prevInter=((-abs(horIn)+abs(s[top].horizIntersect))/(long double)(abs(s[top].slope)-abs(slo)));
+					s[top+1]=particle;
+					top++;
 				}
 			}
 		}
-	return StackA;
+		
+	return;
 }
 int main(){
-	long long int k;
-	long int n;
-	long long int l;
-	long long int u,t;
+	short int flag, count,flag1,flag2;
+	long long int k,n,l,ptrA,ptrB,orderA=0,orderB=0,k1,j,ind1,ind2;
+	long double x1,x2,t;
 	//prevInter holds the previous time that 2 lines intersect
-	cin >> n >>l>>k;
-	//find for each section which particle from A is first O(n)
+	cin >> n >>l>>k1;
+	input *inA= new input [n];
+	input *inB= new input [n];
+	line *stackA = new line [n];
+	line *stackB = new line [n];
 	for (long long int i=0; i<n; i++){
-		cin>>t>>u;
-		StackA=fillStack(i,t,u);
+		cin>>inA[i].t>>inA[i].u;
+		inA[i].relevant=1;
 	}
-	//find for each section which particle from B is first O(n)
 	for (long long int i=0; i<n; i++){
-		cin>>t>>u;
-		StackB=fillStack(i,t,u);
+			cin>>inB[i].t>>inB[i].u;
+			inB[i].relevant=1;
 	}
-	printStack(StackA);
-	printStack(StackB);
+	for (long long int j1=0; j1<k1; j1++){
+		flag=0; 
+
+		//find for each section which particle from A is first O(n)
+		for (long long int i=0; i<n; i++){
+			stackA[i].order=0;
+			stackB[i].order=0;
+			if(inA[i].relevant){
+				particle.order=i+1;
+				particle.slope=inA[i].u;
+				particle.horizIntersect=inA[i].t*inA[i].u;
+				fillStack(stackA,particle.slope,particle.horizIntersect);
+			}
+		}
+		long long int i;
+		ptrA=0;long long int endptrA=top;
+		//find for each section which particle from B is first O(n)
+		for (long long int i=0; i<n; i++){
+			if(inB[i].relevant){
+				particle.order=i+1;
+				particle.slope=-inB[i].u;
+				particle.horizIntersect=-l-inB[i].t*inB[i].u;
+				fillStack(stackB,particle.slope,particle.horizIntersect);
+			}
+		}
+		ptrB=top;
+		long long int endptrB=top;
+
+		i=0 ; 
+        long long int k=0;
+		long double *time = new long double [endptrB+endptrA+2];
+
+        i=0;
+        j=0;
+        // merge the 2 arrays to 1 (asc)
+		while(stackA[i].order!=0 && stackB[j].order!=0){
+			
+			if(stackA[i].prevInter<stackB[j].prevInter){
+				time[k]=stackA[i].prevInter;
+				k++;
+				i++;
+			}
+			else if(stackA[i].prevInter>stackB[j].prevInter){
+				time[k]=stackB[j].prevInter;
+				k++;
+				j++;
+			}
+            else{
+                time[k]=stackB[j].prevInter;
+                k++;
+                i++;
+                j++;
+            }
+		}
+		if (stackA[i].order==0){
+			while(stackB[j].order!=0){
+				time[k]=stackB[j].prevInter;
+				k++;
+				j++;
+			}
+		}
+		else
+		{
+			while(stackA[i].order!=0){
+				time[k]=stackA[i].prevInter;
+				k++;
+				i++;
+			}
+		}
+        i = 0; j = 0;
+		//find the first conflict
+		for (int lol=0; lol<k; lol++)
+		{
+			t = time[lol];
+
+            flag1 = 0;
+            flag2 = 0;
+            if (stackA[i+1].order!=0 && stackA[i+1].prevInter<=t){
+                flag1 = 1;
+                i++;
+            }
+            if (stackB[j+1].order!=0 && stackB[j+1].prevInter<=t){
+                flag2 = 1;
+                j++;
+            }
+           	ind1 = stackA[i].order -1;
+           	ind2 = stackB[j].order -1;
+
+            x1 = inA[ind1].u * (t - inA[ind1].t);
+            x2 = l-1.0*inB[ind2].u * (t - inB[ind2].t);
+
+            if (x1>x2) 
+                break;
+            flag1 = 0;
+            flag2 = 0;
+            
+		}
+			if (flag1 && !flag2){
+				ind1 = stackA[i-1].order-1;
+				ind2 = stackB[j].order -1;
+			}
+			else if (flag2 && !flag1){
+				ind1 = stackA[i].order - 1;
+				ind2 = stackB[j-1].order - 1;
+			}
+			else {
+				ind1 = stackA[i].order-1;
+				ind2 = stackB[j].order -1;
+			}
+			cout<<ind1+1<<" "<<ind2+1<<"\n";
+			inA[ind1].u=0;
+			inB[ind2].u=0;
+	}
 	return 0;
 }
