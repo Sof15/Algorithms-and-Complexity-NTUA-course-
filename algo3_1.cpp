2b13a098523edd5aa460bcd6d8b3532719183e3a
Sof15 <sof.galanaki@gmail.com>
2018-12-29 22:07:24 +0200

    submarine done O(nmx)

--- algo3_1.cpp
+++ algo3_1.cpp
@@ -1,30 +1,40 @@
 #include <iostream>
-#include <queue>
 #include <string>
 #include <unordered_map>
-#include <vector>
 
 using namespace std;
 struct vertex
 {
-	int x, y, endX, endY;
-};
+	long long int  endX, endY;
+}v;
 
-long long int rec(int i, int j, int h, long long int *arr, unordered_map<string, vertex> bridgesStart, int n, int m)
+unordered_map <string, vertex> bridgesStart;
+long long int *arr;
+
+long long int rec(int i, int j, int h, int n, int m, int x)
 {
+	
+	//cout<<i<<" "<<j<<" "<<arr[((i*m+j)*(x+1)+h)]<<"\n";
 
-	int sum=0;
+	long long int sum=0;
+
 	if((i>n-1 || i<0)|| (j>m-1 || j<0) || h<0){return 0;}
 	else if(i==0 && j==0){return 1;}
-	else if(bridgesStart.find(to_string(i)+to_string(j))!=bridgesStart.end()){
-		//if it's the end of a passage 
-		sum += rec(bridgesStart.find(to_string(i)+to_string(j))->second.endX,bridgesStart.find(to_string(i)+to_string(j))->second.endY, h-1, arr, bridgesStart,n,m);
+	else if(arr[(i*m+j)*(x+1)+h]>-1){return arr[(i*m+j)*(x+1)+h]%1000000103;}
+	else if(bridgesStart.find(to_string(i)+" "+to_string(j))!=bridgesStart.end()){
+		//if it's the start of a passage 
+		sum += rec(bridgesStart.find(to_string(i)+" "+to_string(j))->second.endX,bridgesStart.find(to_string(i)+" "+to_string(j))->second.endY, h-1, n,m,x)%1000000103;
+		sum=sum%1000000103;
 	}
 	else{
 		//cout<<i<<" "<<j<<"\n";
-		sum+=rec(i-1,j, h, arr, bridgesStart, n,m);
-		sum+=rec(i,j-1,h, arr, bridgesStart, n,m);
+		sum+=rec(i-1,j, h, n,m,x)%1000000103;
+		sum=sum%1000000103;
+		sum+=rec(i,j-1,h,n,m,x)%1000000103;
+		sum=sum%1000000103;
 	}
+
+	arr[(i*m+j)*(x+1)+h]=sum;
 
 	return sum;
 }
@@ -35,25 +45,29 @@ int main()
 	cin.tie(static_cast<ostream*>(0));
 	int n,m,k,x;
 	long long int start, end;
-	long long int *arr;
-	cin >> n>>m>>k>>x;
-	arr=new long long int [n * m * x];
 	
-	unordered_map <string, vertex> bridgesStart;
-	//unordered_map <string, vertex> bridgesEnd;
-	vertex v;
-	for (int i=0; i<k; i++){
+	cin >> n>>m>>k>>x;
+	arr=new long long int [n * m * (x+1)];
+	
+	
+	for ( int i=0; i<k; i++){
 		cin >> start >> end;
-		v.x=start/m;
-		v.y=start%m;
 		v.endX=end/m;
 		v.endY=end%m;
-		cout<<v.x<<" "<<v.y<<v.endX<<" "<<v.endY<<"\n";
-		bridgesStart[to_string(v.x)+to_string(v.y)]=v;
-	//	bridgesEnd[to_string(v.endX)+to_string(v.endY)]=v;
+		
+		bridgesStart[to_string(start/m)+" "+to_string(start%m)]=v;
+
 	}
 
-	int tot=rec(n-1,m-1,x, arr,bridgesStart,n,m);
+	for(int i=0; i<n; i++){
+		for(int j=0; j<m; j++){
+			for(int k=0; k<x+1; k++){
+				arr[(i*m+j)*(x+1)+k]=-1;
+			}
+		}
+	}
+
+	long long int tot=rec(n-1,m-1,x, n,m,x)%1000000103;
 	cout <<tot<<"\n";
 	return 0;
 }
